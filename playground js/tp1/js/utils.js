744d04088c5e1e4cc4d06b7b56678f3e3803de5a
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2026-04-27 15:44:50 +0100

    ok

---
+++ playground js/tp1/js/utils.js
@@ -0,0 +1,41 @@
+// ajouter les donnes d un etudiant a la table html
+const addStudentToTable = student => {
+    const tr = document.createElement('tr')
+    const tdCne = document.createElement('td')
+    const tdFirstName = document.createElement('td')
+    const tdLastName = document.createElement('td')
+    const tdDateBirth = document.createElement('td')
+    const tdActions = document.createElement('td')
+    const span = document.createElement('span')
+    const button = document.createElement('button')
+    // parent.append(fils)
+    tr.append(tdCne, tdFirstName, tdLastName, tdDateBirth, tdActions)
+    tdCne.append(span)
+    tdActions.append(button)
+    tbody.append(tr)//document.getElementById("tbody") optional
+
+    span.textContent = student.cne
+    tdFirstName.textContent = student.firstName
+    tdLastName.textContent = student.lastName
+    tdDateBirth.textContent = student.dateOfBirth
+    button.textContent = "Delete"
+
+    span.classList.add("badge")
+    tdActions.classList.add("td-actions")
+    button.classList.add("btn", "btn-danger", "btn-sm")
+
+    button.setAttribute("type", "button")
+
+}
+
+const addStudentToTable2 = (student) => { 
+    tbody.innerHTML += `<tr>
+                                <td><span class="badge">${student.cne}</span></td>
+                                <td>${student.firstName}</td>
+                                <td>${student.lastName}</td>
+                                <td>${student.dateOfBirth}</td>
+                                <td class="td-actions">
+                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                </td>
+                            </tr>`;
+}
